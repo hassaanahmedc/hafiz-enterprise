b44d1390ab92bc70fa30514f886cc7a74cf03b8f
Hassaan Ahmed <hassaanahmedc@gmail.com>
2025-01-13 21:51:55 +0500

    optimized the images

--- script.js
+++ script.js
@@ -1,60 +1,62 @@
-const menuToggle = document.getElementById('menu-toggle');
-const slidingMenu = document.getElementById('sliding-menu');
-const closeMenuButton = document.getElementById('close-menu');
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const slidingMenu = document.getElementById('sliding-menu');
+    const closeMenuButton = document.getElementById('close-menu');
 
-// Mobile Toggle Close
-menuToggle.addEventListener('change', () => {
-    if (menuToggle.checked) {
-        slidingMenu.classList.remove('-translate-x-full');
-    } else {
-        slidingMenu.classList.add('-translate-x-full');
-    }
-});
-
-// Mobile Toggle Close
-closeMenuButton.addEventListener('click', () => {
-    slidingMenu.classList.add('-translate-x-full');
-    menuToggle.checked = false;
-});
-
-// Sticky WhatsApp Button on scrolll
-const whatsappButton = document.getElementById('whatsapp-button');
-const heroBtn = document.getElementById('hero-contact-btn');
-
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-
-            whatsappButton.classList.add('hidden');
+    // Mobile Toggle Close
+    menuToggle.addEventListener('change', () => {
+        if (menuToggle.checked) {
+            slidingMenu.classList.remove('-translate-x-full');
         } else {
-
-            whatsappButton.classList.remove('hidden');
+            slidingMenu.classList.add('-translate-x-full');
         }
     });
-}, {
-    threshold: 0.1,
-});
 
-observer.observe(heroBtn);
-
-// Smooth Scrolling
-const sections = document.querySelectorAll('[data-anime]'); // Targeting elements with data-anime
-
-const scrollObserver = new IntersectionObserver((entries, scrollObserver) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const animationType = entry.target.getAttribute('data-anime'); // Get the animation type
-
-            // Add the 'visible' class to trigger the animation
-            entry.target.classList.add('visible');
-            
-            // Stop observing once the animation is triggered
-            scrollObserver.unobserve(entry.target);
-        }
+    // Mobile Toggle Close
+    closeMenuButton.addEventListener('click', () => {
+        slidingMenu.classList.add('-translate-x-full');
+        menuToggle.checked = false;
     });
-}, {
-    threshold: 0.2, // Adjust threshold based on when you want the animation to trigger
-});
 
-// Observe each section
-sections.forEach(section => scrollObserver.observe(section));
+    // Sticky WhatsApp Button on scrolll
+    const whatsappButton = document.getElementById('whatsapp-button');
+    const heroBtn = document.getElementById('hero-contact-btn');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+
+                whatsappButton.classList.add('hidden');
+            } else {
+
+                whatsappButton.classList.remove('hidden');
+            }
+        });
+    }, {
+        threshold: 0.1,
+    });
+
+    observer.observe(heroBtn);
+
+    // Smooth Scrolling
+    const sections = document.querySelectorAll('[data-anime]'); // Targeting elements with data-anime
+
+    const scrollObserver = new IntersectionObserver((entries, scrollObserver) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const animationType = entry.target.getAttribute('data-anime'); // Get the animation type
+
+                // Add the 'visible' class to trigger the animation
+                entry.target.classList.add('visible');
+
+                // Stop observing once the animation is triggered
+                scrollObserver.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.2, // Adjust threshold based on when you want the animation to trigger
+    });
+
+    // Observe each section
+    sections.forEach(section => scrollObserver.observe(section));
+});
